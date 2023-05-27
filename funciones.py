07a020c17cfa4ce0b5eafaa24d86c788e8be137e
AlexisAguinagalde0 <aguinagaldealexis98@gmail.com>
2023-05-27 12:53:16 -0300

    Punto 3 Agregado

--- funciones.py
+++ funciones.py
@@ -6,3 +6,27 @@ def cargar_datos():
         jugadores = datos["jugadores"]
     return jugadores
 
+def mostrar_datos(datos_jugadores):
+    for jugador in datos_jugadores:
+        print(f"{jugador['nombre']} - {jugador['posicion']}")
+        
+def mostrar_estadisticas_jugador(datos):
+    for contador, jugador in enumerate(datos_jugadores, start=1):
+        print(contador, jugador["nombre"])
+    indice = int(input("Ingrese el índice del jugador: "))
+    if not (indice < 0 or indice >= len(datos_jugadores)):
+        jugador = datos[indice]
+        jugadorindice = jugador["estadisticas"]
+        for clave, valor in jugadorindice.items():
+            clave_formateada = (str(clave).replace("_", " ")).capitalize()
+            print(f"{clave_formateada}: {valor}")
+    else:    
+        print("¡Índice inválido!")
+        
+
+def guardar_estadisticas_csv(datos):
+    indice = int(input("Ingrese el índice del jugador: "))
+    if indice < 0 or indice >= len(datos):
+        print("¡Índice inválido!")
+        return
+    jugador = datos[indice]
